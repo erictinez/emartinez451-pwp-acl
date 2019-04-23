ac50bdeb8221adf0665b881c126a4e8d126c0ed2
Eric Martinez <emartinez451@cnm.edu>
2019-04-23 14:18:07 -0600

    milestone 1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PERSONAL WEBSITE PROJECT</title>
+	</head>
+
+	<body>
+		<h1>PURPOSE, AUDIENCE, GOAL</h1>
+		<p><strong>PURPOSE:</strong></p><p> This project is the creation for a professional portfolio website.</p>
+		<p><strong>AUDIENCE:</strong></p><p> This site is designed for a professional audience and is targeted to individuals and small businesses</p>
+		<p><strong>GOAL:</strong></p><p> The primary goal of this website is to create a point of contact for prospective clients and/or collaborators.</p>
+		<h2>PERSONA</h2>
+		<p><strong>NAME:</strong></p><p>Anthony Chin Loy</p>
+		<p><strong>AGE:</strong></p><p>37</p>
+		<p><strong>PROFESSION:</strong></p><p> MD of Otorhinolaryngology</p>
+		<p><strong>TECHNOLOGY:</strong><p>iPhone 6+ on an unlimited 4G connection, self-built PC running macOS Sierra, LG ultra-wide monitor.</p>
+		<p><strong>ATTITUDES AND BEHAVIORS:</strong><p> Anthony is a busy medical professional who values family time.
+		He works hard at balancing his professional and personal life.
+		He also maintains a social media presence; he uses Facebook and Instagram for his personal life and Doximithy for professional networking.</p>
+		<p><strong>FRUSTRATIONS AND NEEDS:</strong></p><p> Anthony is unhappy with the "build your own websites" and is looking for a professional web developer to create a interactive website tailored to his needs.</p>
+		<p><strong>GOALS:</strong> Anthony's goal is to find a skilled web developer to build a website that will develop an online following to his product reviews and lifestyle suggestions.</p>
+		<p><strong>USER STORY:</strong></p><p> As a user, I want a web developer who meets my needs.</p>
+
+	</body>
