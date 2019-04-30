b601f6c0d7b6a00cc3db7e51d1addd13d7c7ee4c
Eric Martinez <emartinez451@cnm.edu>
2019-04-30 15:57:31 -0600

    added mobile wireframe to pwp project

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP Milestone Two</title>
+	</head>
+
+	<body>
+		<h1>Content Strategy</h1>
+
+		<p>My site will be one page and will contain the following
+			sections.</p>
+
+		<p><strong>Welcome:</strong> This will be a short introduction of Anthony Chin Loy.</p>
+
+		<p><strong>About Me:</strong> This section of the PWP will include
+			educational background for Anthony and his awards and acknowledgements.
+			This section will introduce his purpose for the website being a product review and
+			lifestyle suggestion platform.</p>
+
+		<p><strong>Portfolio:</strong> Anthony's portfolio will include links to products he ha
+			reviewed. It will also show a sample of some lifestyle suggestions.(At least two examples of each
+		    and will include an interactive image gallery.</p>
+
+		<p><strong>Contact:</strong> This section will contain a simple contact form that
+			allows prospective clients to contact Anthony. It will also include Anthony's
+			professional email and links his professional Doximity profile and social media profiles.</p>
+
+		<img src="mobilewf.jpg" alt="mobile-wire-frame">
+
+		<img src="desktopwf.jpg" alt="desktop-wire-frame">
+
+	</body>
