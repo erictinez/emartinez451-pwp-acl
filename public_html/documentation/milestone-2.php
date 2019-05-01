46413827156373a4257dcd4e1bffdf75d6138b97
Eric Martinez <emartinez451@cnm.edu>
2019-05-01 07:36:18 -0600

    added wireframes to pwp

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -26,8 +26,8 @@
 			allows prospective clients to contact Anthony. It will also include Anthony's
 			professional email and links his professional Doximity profile and social media profiles.</p>
 
-		<img src="mobilewf.jpeg" alt="mobile-wire-frame>
+		<img src="mobilewf.jpg" alt="mobile-wire-frame>
 
-		<img src="pwpdesktop.jpeg" alt="mobile-wire-frame>
+		<img src="desktopwf.jpg" alt="mobile-wire-frame>
 
 	</body>
