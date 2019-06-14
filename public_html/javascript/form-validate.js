96d0017b257fe02c094330d9deb49f3576d9cde7
Eric Martinez <emartinez451@cnm.edu>
2019-06-13 21:16:49 -0600

    removed navbar and fixed contact form

--- public_html/javascript/form-validate.js
+++ public_html/javascript/form-validate.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 
 		// setup handling of form errors
 		debug: true,
-		errorClass: "alert alert-danger",
+		errorClass: "",
 		errorLabelContainer: "#output-area",
 		errorElement: "div",
 
